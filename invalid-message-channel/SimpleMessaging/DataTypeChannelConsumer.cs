3ed37a78f6acc9267f174ce643f118149c9e9cce
Matthias Heylen <heylenmatthias@gmail.com>
2020-06-08 15:55:51 +0200

    Finished invalid Message channel

--- invalid-message-channel/SimpleMessaging/DataTypeChannelConsumer.cs
+++ invalid-message-channel/SimpleMessaging/DataTypeChannelConsumer.cs
@@ -6,7 +6,7 @@ using RabbitMQ.Client;
 
 namespace SimpleMessaging
 {
-    public class DataTypeChannelConsumer<T> : IDisposable where T: IAmAMessage
+    public class DataTypeChannelConsumer<T> : IDisposable where T : IAmAMessage
     {
         private readonly Func<string, T> _messageDeserializer;
         private readonly string _queueName;
@@ -42,28 +42,37 @@ namespace SimpleMessaging
             factory.AutomaticRecoveryEnabled = true;
             _connection = factory.CreateConnection();
             _channel = _connection.CreateModel();
-            
-           
-             /* We choose to base the key off the type name, because we want tp publish to folks interested in this type
-              We name the queue after that routing key as we are point-to-point and only expect one queue to receive
-             this type of message */
+
+
+            /* We choose to base the key off the type name, because we want tp publish to folks interested in this type
+             We name the queue after that routing key as we are point-to-point and only expect one queue to receive
+            this type of message */
             var routingKey = "Invalid-Message-Channel." + typeof(T).FullName;
             _queueName = routingKey;
 
             var invalidRoutingKey = "invalid." + routingKey;
             var invalidMessageQueueName = invalidRoutingKey;
-            
+
             //TODO create an argument dictionary, that has arguments for the invalid message exchange and routing key
-           
+            var arguments = new Dictionary<string, object>()
+            {
+                { "x-dead-letter-exchange", InvalidMessageExchangeName},
+                { "x-dead-letter-routing-key", invalidRoutingKey}
+            };
             //TODO: Create our consumer queue, but add the arguments that hook up the invalid message queue (tip might be calle deal letter in RMQ docs)
-            _channel.QueueBind(queue:_queueName, exchange: ExchangeName, routingKey: routingKey);
-            
+            _channel.ExchangeDeclare(exchange: ExchangeName, ExchangeType.Direct, durable: false);
+            _channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: arguments);
+            _channel.QueueBind(queue: _queueName, exchange: ExchangeName, routingKey: routingKey);
+
             //declare a queue for invalid messages off an invalid message exchange
             //messages that we nack without requeue will go here
             // TODO; Declare an invalid message queue exchange, direct and durable
+            _channel.ExchangeDeclare(exchange: InvalidMessageExchangeName, ExchangeType.Direct, durable: true);
             // TODO: declare an invalid message queue, durable
+            _channel.QueueDeclare(queue: invalidMessageQueueName, durable: true, exclusive: false, autoDelete: false);
             // TODO: bind the queue to the exchange
- 
+            _channel.QueueBind(queue: invalidMessageQueueName, exchange: InvalidMessageExchangeName, routingKey: invalidRoutingKey);
+
         }
 
         /// <summary>
@@ -79,18 +88,18 @@ namespace SimpleMessaging
                 try
                 {
                     T message = _messageDeserializer(Encoding.UTF8.GetString(result.Body));
-                    _channel.BasicAck(deliveryTag:result.DeliveryTag, multiple: false);
+                    _channel.BasicAck(deliveryTag: result.DeliveryTag, multiple: false);
                     return message;
                 }
                 catch (JsonSerializationException e)
                 {
                     Console.WriteLine($"Error processing the incoming message {e}");
                     //put format errors onto the invalid message queue
-                    _channel.BasicNack(deliveryTag:result.DeliveryTag, multiple: false, requeue:false);
+                    _channel.BasicNack(deliveryTag: result.DeliveryTag, multiple: false, requeue: false);
                 }
-            
-            return default(T) ;
-        }   
+
+            return default(T);
+        }
 
         public void Dispose()
         {
